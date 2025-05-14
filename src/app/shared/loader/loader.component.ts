364cd37ea209e2807f1951edbe0dc8859be7de7c
Aadarsh-ops <paradisegamer649@gmail.com>
2025-05-14 07:00:30 +0530

    fix-bug

--- src/app/shared/loader/loader.component.ts
+++ src/app/shared/loader/loader.component.ts
@@ -6,7 +6,7 @@ import { LoaderService } from '../service/loader.service';
 @Component({
   selector: 'app-loader',
   templateUrl: './loader.component.html',
-  styleUrls: ['./loader.component.css']
+  styleUrls: ['./loader.component.scss']
 })
 export class LoaderComponent {
   isLoading$: Observable<boolean>;
